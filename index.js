15a2e1b5c70772afe28c4761565c97da88fc034a
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-06-29 16:55:03 +0200

    All properties of the original JSON file has been added to the new object

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var xml2js = require('xml2js')
 
 var inputfile = __dirname + '/restnoterat.xml'
 var outputfile =  __dirname + '/restnoterat/public/supplyshortage.json'
-// läs in data filen, t.ex. restnoteringar.xml
+
+// Reading the XML file
 var data = fs.readFileSync(inputfile, 'utf8')
 console.log(typeof(data))
 
@@ -20,27 +21,48 @@ var obj = {
 var i = 'npl1'
 console.log(obj[i].date)
 
-// instansera parser
+// initiate parser
 var parser = new xml2js.Parser();
 parser.parseString(data, function (err, result) {
   var resultObject = {}
   for (var s of result.supplyshortages.supplyshortage) {
     
-    var nplid = s.medicinalproducts[0].medicinalproduct[0].nplid
+    var nplId = s.medicinalproducts[0].medicinalproduct[0].nplid
     var packs = s.medicinalproducts[0].medicinalproduct[0].packages[0].package
+    var startDate = s.medicinalproducts[0].medicinalproduct[0]['forecast-start-date']
+    var endDate = s.medicinalproducts[0].medicinalproduct[0]['forecast-end-date']
+    var referenceNumber = s.referencenumber
+    var firstPublication = s['first-publication-date']
+    var lastPublication = s['last-publication-date']
+    var advice = s.advice
+    var publicContact = s.publiccontactinfo 
 
-    resultObject[nplid] = {
-      nplid: nplid,
-      packs: packs
+    resultObject[nplId] = {
+      nplId: nplId,
+      packs: packs,
+      referenceNumber: referenceNumber,
+      advice: advice,
+      publicContact: publicContact,
+      publicationDate: {
+        firstPublication: firstPublication,
+        lastPublication: lastPublication
+      },
+      forecastDate: {
+        startDate: startDate,
+        endDate: endDate
+      }
+
+
     }
     
   }
-
+    //console.log(JSON.stringify(result, null, 2))
+    // console.log(JSON.stringify(resultObject, null, 2))
   // resultObject = JSON.stringify(result, true, 2)
   //  console.log(err)
    
   
-  var resultfile = fs.createWriteStream(outputfile)
-  resultfile.write(JSON.stringify(resultObject))
+   var resultfile = fs.createWriteStream(outputfile)
+   resultfile.write(JSON.stringify(resultObject))
 })
 
