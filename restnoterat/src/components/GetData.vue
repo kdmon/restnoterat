8a2235a831bd3ddc7b18eb301f1175f78bedf5a9
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-02 21:31:57 +0200

    Created a normal preliminary table to display the properties from supplyshortage

--- restnoterat/src/components/GetData.vue
+++ restnoterat/src/components/GetData.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-    <h1>COMPONENT</h1>
-    <pre>{{JSON.stringify(allSupplies, null, 2)}}</pre>
-    <div v-for="supply in allSupplies" v-bind:key="supply.id" class="supplies">
-      {{ supply }}
-    </div>
+    
+    <!-- <pre>{{JSON.stringify(allSupplies, null, 2)}}</pre> -->
+    <!-- <div v-for="supply in allSupplies" v-bind:key="supply.id" class="supplies">
+      <p>Advice: {{ supply.advice }}</p>
+      <p>Npldid: {{ supply.nplId }}</p>
+      <p>Packs: {{ supply.packs }}</p>
+      <p>Reference: {{ supply.referenceNumber }}</p>
+      <p>Contact: {{ supply.publicContact }}</p>
+      <br>
+    </div> -->
+     <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Advice</th>
+      <th>Npldid</th>
+      <th>Packs</th>
+      <th>Reference</th>
+      <th>Contact</th>
+    </tr>
+  </thead>
+  <tbody>
+     <tr v-for="supply in allSupplies" v-bind:key="supply.nplId">
+       <td>{{ supply.advice }}</td> 
+       <router-link :to="{path: '/nplid/' + supply.nplId}"><td>{{ supply.nplId }}</td></router-link>
+       <!-- <td v-link=>{{ supply.nplId }} </td> -->
+       <td>{{ supply.packs }}</td>  
+       <td>{{ supply.referenceNumber }}</td>
+       <td>{{ supply.publicContact }}</td>    
+    </tr> 
+   </tbody>
+</table>
+
   </div>
 </template>
 
@@ -12,7 +39,7 @@
 import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: "getData",
+  name: "GetData",
   methods: mapActions(["fetchSupplies"]),
   computed: mapGetters(["allSupplies"]),
   created() {
@@ -21,4 +48,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}</style>
