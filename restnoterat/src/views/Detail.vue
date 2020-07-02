18eb731712e3f990f1a2169242ac8cfc6c639c51
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-02 21:34:27 +0200

    Added an if statement to see if the searchquery exist in an object containing nplids

--- restnoterat/src/views/Detail.vue
+++ restnoterat/src/views/Detail.vue
@@ -1,16 +1,26 @@
 <template>
+
   <div>
-    <h1>{{$route.params.id}}</h1>
-   
+    
+    <h4>Details</h4>
+    <div v-if="allSupplies[$route.params.id]">
+      <h4 v-for="(supply, index) in allSupplies[$route.params.id]" v-bind:key="index">{{ supply }}</h4>
+    </div>
+    <h4 v-else>Can't find the nplid you are searching for</h4>
   </div>
 </template>
 
 <script>
-
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: "Detail",
-  
+  name: "GetData",
+  methods: mapActions(["fetchSuppliesTwo"]),
+  computed: mapGetters(["allSupplies"]),
+  created() {
+    this.fetchSuppliesTwo();
+    console.log(this.allSupplies[19930528000047])
+  }
 };
 </script>
 
