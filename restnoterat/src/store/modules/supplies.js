8b69b35fb155e368ee30bedbbb663a718d7302e9
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-07 13:02:11 +0200

    the object for details is complete

--- restnoterat/src/store/modules/supplies.js
+++ restnoterat/src/store/modules/supplies.js
@@ -6,16 +6,50 @@ const state = {
   products: {}
 }
 
+/*
+Receives the state and then brings out the object of a specific ID
+the ID is a nplid is based on the route params from /npld:id.
+Then an object is created containing data from two APIs
+describing a product.
+*/
 const getters = {
   allSupplies: state => state.shortages,
   productByNplId: (state) => (id) => {
-    let o = {}
-    o.name = state.products[id].name
-    o.advice = state.shortages[id].advice
-    o.atc = state.products[id].atc
-    return o
+    const product = {}
+    // Data from products state
+    product.name = state.products[id].name
+    product.prod = state.products[id].prod
+    product.rx = state.products[id].rx
+    product.flags = state.products[id].flags
+    product.substances = state.products[id].substances
+    product.form = state.products[id].form
+    product.strength = state.products[id].strength
+    product.packages = state.products[id].packages
+    product.atc = state.products[id].atc
+
+    // Data from shortages state
+    product.advice = state.shortages[id].advice
+    product.forecastDate = state.shortages[id].forecastDate
+    product.nplId = state.shortages[id].nplId
+    product.packs = state.shortages[id].packs
+    product.publiccontact = state.shortages[id].publicContact
+    product.publicationDate = state.shortages[id].publicationDate
+    product.referenceNumber = state.shortages[id].referenceNumber
+    return product
   }
 }
+
+/*
+"name": "Celectol Sanofi-Aventis",
+  "prod": "LIC",
+  "rx": "1",
+  "flags": {},
+  "substances": [{"name":"IDE4POF6UARA4VERT1","type":"ACTI","qunit2":"EQ"}],
+  "form": "FICOTA",
+  "strength": "200 mg",
+  "packages": {"20131105100082":{"text":"Licensförpackning","otc":"N"}},
+  "atc": "C07AB08"
+*/
 
 const actions = {
   async fetchSupplies ({ commit }) {
--- restnoterat/src/views/Detail.vue
+++ restnoterat/src/views/Detail.vue
@@ -1,33 +1,42 @@
 <template>
   <div>
     <h4>Details</h4>
-    <div v-if="productByNplId($route.params.id)">
+    <div v-if='productByNplId($route.params.id)'>
       <!-- <h4
         v-for="(supply, index) in allSupplies[$route.params.id]"
         v-bind:key="index"
       >
         {{ supply }}
-        
-      </h4> -->
-      <h4>{{ productByNplId($route.params.id).name }}</h4>
-      <h4>{{ productByNplId($route.params.id).atc }}</h4>
-      <h4>{{ productByNplId($route.params.id).advice }}</h4>
-      
 
+      </h4> -->
+      <h4 v-if='productByNplId($route.params.id).name'>Name: {{ productByNplId($route.params.id).name }}</h4>
+      <h4>Prod: {{ productByNplId($route.params.id).prod }}</h4>
+      <h4>Rx: {{ productByNplId($route.params.id).rx }}</h4>
+      <h4>Flags: {{ productByNplId($route.params.id).flags }}</h4>
+      <h4>Substances: {{ productByNplId($route.params.id).substances }}</h4>
+      <h4>Form: {{ productByNplId($route.params.id).form }}</h4>
+      <h4>Strength: {{ productByNplId($route.params.id).strength }}</h4>
+      <h4>packages: {{ productByNplId($route.params.id).packages }}</h4>
+      <h4>Atc: {{ productByNplId($route.params.id).atc }}</h4>
+      <h4>Advice: {{ productByNplId($route.params.id).advice }}</h4>
+      <h4>Forecast: {{ productByNplId($route.params.id).forecastDate }}</h4>
+      <h4>Npl: {{ productByNplId($route.params.id).nplId }}</h4>
+      <h4>Packs: {{ productByNplId($route.params.id).packs }}</h4>
+      <h4>Contact: {{ productByNplId($route.params.id).publicContact }}</h4>
+      <h4>Publication: {{ productByNplId($route.params.id).publicationDate }}</h4>
+      <h4>Reference: {{ productByNplId($route.params.id).referenceNumber }}</h4>
     </div>
     <h4 v-else>Can't find the nplid you are searching for</h4>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'GetData',
-  methods: mapActions(['fetchSuppliesTwo']),
   computed: mapGetters(['productByNplId']),
   created () {
-    // this.fetchSuppliesTwo();
     // console.log(this.allSupplies[19930528000047])
   }
 }
