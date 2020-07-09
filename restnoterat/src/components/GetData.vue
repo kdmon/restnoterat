4e7b9f6e5a79d697551f719334d0eb93ec4d5388
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-09 11:59:21 +0200

    Adapted the code to match the new object called combined

--- restnoterat/src/components/GetData.vue
+++ restnoterat/src/components/GetData.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <pre> zzzz {{JSON.stringify(shortages(), null, 2)}}</pre>
+    <!-- <pre> zzzz {{JSON.stringify(shortages(), null, 2)}}</pre> -->
     <!-- <div v-for="supply in allSupplies" v-bind:key="supply.id" class="supplies">
       <p>Advice: {{ supply.advice }}</p>
       <p>Npldid: {{ supply.nplId }}</p>
@@ -9,33 +9,34 @@
       <p>Contact: {{ supply.publicContact }}</p>
       <br>
     </div> -->
-
     <table class="table table-striped">
       <thead>
         <tr>
-          <th>Advice</th>
-          <th>Npldid</th>
-          <th>Packs</th>
-          <th>Reference</th>
+          <th>Namn</th>
+          <th>Atc</th>
+          <th>Npl id</th>
+          <th>Referens</th>
+          <th>Förslag</th>
+          <th>Pack</th>
           <th>Contact</th>
-          <th>Publication</th>
-          <th>Forecast</th>
+          <!-- <th>Publication</th> -->
+          <!-- <th>Forecast</th> -->
           <th>Pågående</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="supply in shortages()" v-bind:key="supply.nplId">
-          <td>{{ supply.advice }}</td>
-          <router-link :to="{ path: '/nplid/' + supply.nplId }"
-            ><td>{{ supply.nplId }}</td></router-link
-          >
-          <!-- <td v-link=>{{ supply.nplId }} </td> -->
-          <td>{{ supply.shortages[0].packs }}</td>
-          <td>{{ supply.shortages[0].referenceNumber }}</td>
-          <td>{{ supply.publicContact }}</td>
-          <td>{{ supply.publicationDate }}</td>
-          <td>{{ supply.forecastDate }}</td>
           <td>{{ supply.name }}</td>
+          <td>{{ supply.atc }}</td>
+          <router-link :to="{ path: '/nplid/' + supply.nplId }"
+            ><td>{{ supply.nplId }}</td></router-link>
+            <td>{{ supply.shortages[0].referenceNumber }}</td>
+          <td>{{ supply.shortages[0].advice }}</td>
+          <td>{{ supply.shortages[0].packs }}</td>
+          <td>{{ supply.shortages[0].publicContact }}</td>
+          <!-- <td>{{ supply.shortages[0].publicationDate.firstPublication }}</td> -->
+          <td v-if='supply.currentShortage'>Ja</td>
+          <td v-else>Nej</td>
         </tr>
       </tbody>
     </table>
