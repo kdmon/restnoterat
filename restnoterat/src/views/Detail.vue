50d85eac1981a8a321a19f947ce001701ba484f5
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-13 13:36:18 +0200

    Displayinh details of the chosen object

--- restnoterat/src/views/Detail.vue
+++ restnoterat/src/views/Detail.vue
@@ -2,25 +2,25 @@
   <div>
     <h4>Details</h4>
     <div v-if='productByNplId($route.params.id)'>
-      <h4 v-if='productByNplId($route.params.id).name'>Name: {{ productByNplId($route.params.id).name }}</h4>
-      <h4>Prod: {{ productByNplId($route.params.id).prod }}</h4>
-      <h4>Rx: {{ productByNplId($route.params.id).rx }}</h4>
-      <h4>Flags: {{ productByNplId($route.params.id).flags }}</h4>
-      <h4>Substances: {{ productByNplId($route.params.id).substances }}</h4>
-      <h4>Form: {{ productByNplId($route.params.id).form }}</h4>
-      <h4>Strength: {{ productByNplId($route.params.id).strength }}</h4>
-      <h4>packages: {{ productByNplId($route.params.id).packages }}</h4>
-      <h4>Atc: {{ productByNplId($route.params.id).atc }}</h4>
-      <h4>Advice: {{ productByNplId($route.params.id).advice }}</h4>
-      <h4>Forecast: {{ productByNplId($route.params.id).forecastDate }}</h4>
-      <h4>Npl: {{ productByNplId($route.params.id).nplId }}</h4>
-      <h4>Packs: {{ productByNplId($route.params.id).packs }}</h4>
-      <h4>Contact: {{ productByNplId($route.params.id).publicContact }}</h4>
-      <h4>Publication: {{ productByNplId($route.params.id).publicationDate }}</h4>
-      <h4>Reference: {{ productByNplId($route.params.id).referenceNumber }}</h4>
-      <pre>
+      <h6 v-if='productByNplId($route.params.id).name'>Name: {{ productByNplId($route.params.id).name }}</h6>
+      <h6>Prod: {{ productByNplId($route.params.id).prod }}</h6>
+      <h6>Rx: {{ productByNplId($route.params.id).rx }}</h6>
+      <h6>Flags: {{ productByNplId($route.params.id).flags }}</h6>
+      <h6>Substances: {{ productByNplId($route.params.id).substances }}</h6>
+      <h6>Form: {{ productByNplId($route.params.id).form }}</h6>
+      <h6>Strength: {{ productByNplId($route.params.id).strength }}</h6>
+      <h6>packages: {{ productByNplId($route.params.id).packages }}</h6>
+      <h6>Atc: {{ productByNplId($route.params.id).atc }}</h6>
+      <h6>Advice: {{ productByNplId($route.params.id).shortages[0].advice }}</h6>
+      <h6>Forecast: {{ productByNplId($route.params.id).shortages[0].forecastDate }}</h6>
+      <h6>Npl: {{ productByNplId($route.params.id).nplId }}</h6>
+      <h6>Packs: {{ productByNplId($route.params.id).shortages[0].packs }}</h6>
+      <h6>Contact: {{ productByNplId($route.params.id).shortages[0].publicContact }}</h6>
+      <h6>Publication: {{ productByNplId($route.params.id).shortages[0].publicationDate }}</h6>
+      <h6>Reference: {{ productByNplId($route.params.id).shortages[0].referenceNumber }}</h6>
+      <!-- <pre>
         {{ JSON.stringify(productByNplId($route.params.id).shortages, null, 2) }}
-      </pre>
+      </pre> -->
     </div>
     <h4 v-else>Can't find the nplid you are searching for</h4>
   </div>
