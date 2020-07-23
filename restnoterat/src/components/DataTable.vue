4c938fbb448cb2de2d50f383fc66714edb4ecfc4
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-23 16:35:59 +0200

    Started creating a timetable for shortages

--- restnoterat/src/components/DataTable.vue
+++ restnoterat/src/components/DataTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <pre> zzzz {{JSON.stringify(shortages(), null, 2)}}</pre> -->
-    <table class="table table-striped">
+    <!-- <table class="table table-striped">
       <thead>
         <tr>
           <th>Namn</th>
@@ -11,8 +11,6 @@
           <th>Förslag</th>
           <th>Pack</th>
           <th>Contact</th>
-          <!-- <th>Publication</th> -->
-          <!-- <th>Forecast</th> -->
           <th>Pågående</th>
         </tr>
       </thead>
@@ -27,12 +25,35 @@
           <td>{{ supply.shortages[0].advice }}</td>
           <td>{{ supply.shortages[0].packs }}</td>
           <td>{{ supply.shortages[0].publicContact }}</td>
-          <!-- <td>{{ supply.shortages[0].publicationDate.firstPublication }}</td> -->
           <td v-if="supply.currentShortages.length > 0">Ja</td>
           <td v-else>Nej</td>
         </tr>
       </tbody>
-    </table>
+    </table>-->
+    <!-- <div class="row">
+      <div class="col-1 column atc">
+        <h4>ssdada</h4>
+        <h4>ssdada</h4>
+      </div>
+      <div class="col-11 column">
+        <p>dsadad</p>
+        <p>dsadad</p>
+        <p>dsadad</p>
+      </div>
+    </div>-->
+    <div class="row" v-for="supply in shortages()" :key="supply.nplId" style="margin-top: 0">
+      <div class="col-1">
+        <h5 style="width: fit-content">{{ supply.atc }}</h5>
+      </div>
+      <div class="col-11">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <rect x="0" y="0" width="600" height="100" fill="red" />
+            <text x="0" y="50" fill="black">{{ supply.shortages[0].packs }}</text>
+          </g>
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,5 +76,25 @@ th,
 td {
   border: 1px solid black;
   border-collapse: collapse;
+}
+.my-card {
+  max-width: 400px;
+}
+.row > div {
+  padding: 10px 15px;
+  background: rgba(86, 61, 124, 0.15);
+  border: 1px solid rgba(86, 61, 124, 0.2);
+}
+.row + .row {
+  margin-top: 1rem;
+}
+.headline {
+  width: 600px;
+}
+.medicinalProduct {
+  font-size: 20px;
+}
+.atc {
+  border: 4px solid yellow;
 }
 </style>
