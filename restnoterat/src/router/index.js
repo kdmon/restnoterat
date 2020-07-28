24092c2005e0308618e54f7447132176810e92b5
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2020-07-28 16:34:49 +0200

    Added new route for an about view

--- restnoterat/src/router/index.js
+++ restnoterat/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Detail from '../views/Detail.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/nplid/:id',
     name: 'Detail',
     component: Detail
+  },
+  {
+    path: '/om',
+    name: 'About',
+    component: About
   }
 ]
 
---
+++ restnoterat/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="info">
+        <p>Syftet med denna webbplats är att vård- och aptekspersonal samt andra intressenter snabbt
+            och enkelt ska kunna överblicka läkemdel som är, har varit eller förväntas
+            bli tillfälligt slut hos tillverkare.
+
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.info {
+    display: flex;
+    justify-content: center;;
+}
+.info p {
+    max-width: 500px;
+}
+</style>
