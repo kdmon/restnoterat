a858e271d48289e6452031eda5f37715e0aebfa0
kdmon <kdmon@users.noreply.github.com>
2020-08-20 16:29:52 +0200

    Alternative homepage

--- restnoterat/src/router/index.js
+++ restnoterat/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import HomeAlt from '../views/HomeAlt.vue'
 import Detail from '../views/Detail.vue'
 import About from '../views/About.vue'
 import Feedback from '../views/Feedback.vue'
@@ -12,6 +13,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/alt',
+    name: 'Home',
+    component: HomeAlt
   },
   {
     path: '/nplid/:id',
---
+++ restnoterat/src/views/HomeAlt.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+        <div class="row">
+            <div class="col">
+                <div class="q-gutter-md" style="max-width: 300px">
+                    <q-input outlined v-model="query" debounce="500" label="Sök läkemedel" hint="Produktnamn eller atc-kod"/>
+                </div>
+            </div>
+            <div class="col">
+                <q-toggle debounce="500" v-model="currentShortage" label="Enbart pågående restnoteringar"/>
+                <br>
+                <q-toggle debounce="500" v-model="sortAtc" label="Sortera på ATC-kod"/>
+          </div>
+        </div>
+        <br>
+        <graph :current-shortage="currentShortage" :query="query"/>
+  </div>
+</template>
+
+<script>
+import Graph from '@/components/Graph.vue'
+
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      query: '',
+      sortAtc: false,
+      currentShortage: true
+    }
+  },
+  components: {
+    Graph
+  }
+}
+</script>
+
+<style scoped>
+</style>
